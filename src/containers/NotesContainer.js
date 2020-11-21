812e741b51ae987a3521c9a6173da7071322c89c
https://learn.co/arf2k <foreman.alexander@gmail.com>
2020-11-21 16:09:15 -0500

    initial commit

---
+++ src/containers/NotesContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NotesForm from '../components/NotesForm.js'
+
+const NotesContainer = () => {
+     return (
+          <>
+     <h1>Inside Notes Container</h1>
+     <NotesForm/>
+     </>
+     )
+
+}
+
+export default NotesContainer
